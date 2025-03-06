3fb4e239c0330de96513253677ff686c94d86ee4
Tony Otim <tonyotim@Tonys-MacBook-Pro.local>
2025-03-06 11:16:33 -0600

    Initial commit

--- src/main/java/com/otimtony/Main.java
+++ src/main/java/com/otimtony/Main.java
@@ -1,7 +1,23 @@
 package com.otimtony;
 
+import com.otimtony.Model.Patient;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        var welcomeMessage = "Welcome to SmartOnline Appointment Management System (SOAMS)";
+        System.out.printf("%s", welcomeMessage);
+
+        var patients = List.of(
+                new Patient(1, "Tony", "Otim", "6412330895", "otimtonyjeff@gmail.com", "Fairfield", LocalDate.of(1993, 7, 17), 0.0),
+                new Patient(1, "Robin", "Kilama", "6412330999", "kilamaRobin@gmail.com", "Kamplala", LocalDate.of(1990, 11, 17), 0.0),
+                new Patient(1, "Brian", "Akera", "6419870895", "brianakera@gmail.com", "", LocalDate.of(1987, 1, 18), 15.0)
+        );
+
+        patients.forEach(System.out::println);
     }
 }
+
--- src/main/java/com/otimtony/Model/Patient.java
+++ src/main/java/com/otimtony/Model/Patient.java
@@ -1,5 +1,23 @@
 package com.otimtony.Model;
 
-@
-public class Patients {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Patient {
+
+    private Integer id;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
+    private String email;
+    private String address;
+    private LocalDate date_of_birth;
+    private double outstanding_bill;
+
 }
